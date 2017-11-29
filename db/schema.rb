9ae901b7717ec557946860e255737c2b8de52a3b
Hideto Shiozawa <shiorange10593@yahoo.co.jp>
2017-11-29 06:40:36 +0000

    production_is_postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171105075214) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "shiotters", force: :cascade do |t|
     t.string "content"
   end
